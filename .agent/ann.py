bab52aa59f335b4a49d5438b5d34ebb54bd6e1bc
unknown <palacamichaeldale16@gamil.com>
2026-08-26 02:40:10 +0800

    chore: ignore local CI-diagnosis helpers and page dumps

--- .agent/ann.py
+++
@@ -1,8 +0,0 @@
-import io, json, sys
-
-d = json.load(io.open(sys.argv[1], encoding="utf-8"))
-for a in d:
-    lvl = a.get("annotation_level")
-    msg = a.get("message", "")
-    print("=====", lvl)
-    print(msg[:2500])
--- .agent/gh.py
+++
@@ -1,8 +0,0 @@
-import io, json, sys, time
-
-mode = sys.argv[1]
-if mode == "rate":
-    d = json.load(io.open("rl.json", encoding="utf-8"))
-    c = d["resources"]["core"]
-    now = int(time.time())
-    print("remaining", c["remaining"], "reset_in_s", c["reset"] - now)
